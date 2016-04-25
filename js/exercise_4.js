d6f0681e01e8293f22310de6ac02828ddbc723ad
blueones <blueones@users.noreply.github.com>
2016-04-25 11:14:34 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -69,3 +69,26 @@ map.on('click',function(){
 
 })
 
+var myLocation=L.mapbox.featureLayer()
+myLocation.addTo(map);
+
+
+map.on('locationfound',function(e){
+
+  myLocation.setGeoJSON({
+  type:'Feature',
+    geometry:{
+    type:'Point',
+    coordinates:[e.latlng.lng,e.latlng.lat]
+    
+    },
+    properties:{
+    "title":'Here I am!',
+    "marker-color":'#ff8888',
+      "marker-symbol":'star'
+    }
+    
+  })
+  
+})
+map.locate({setView:true});
