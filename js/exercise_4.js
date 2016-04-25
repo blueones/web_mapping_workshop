0c8cd3b1d341de594c65c8c7d50867015549c2d6
blueones <blueones@users.noreply.github.com>
2016-04-25 11:17:07 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -5,8 +5,7 @@
 var mapId = 'will-breitkreutz.k6fj4l3f';
 
 // And this is my access token, use yours.
-var accessToken = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw
-';
+var accessToken = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw';
 
 // Create the map object with your mapId and token,
 // referencing the DOM element where you want the map to go.
