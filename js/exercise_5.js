635ba4ca2b2f0ca2f4954f867b7f02a9ec61d83d
blueones <blueones@users.noreply.github.com>
2016-04-25 10:04:09 -0500

    Updated exercise_5.js

--- js/exercise_5.js
+++ js/exercise_5.js
@@ -1,3 +1,21 @@
+// Here is the javascript setup for a basic map:
+
+// Enter your mapbox map id here to reference it for the base layer,
+// this one references the ugly green map that I made.
+var mapId = 'will-breitkreutz.k6fj4l3f';
+
+// And this is my access token, use yours.
+var accessToken = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw';
+
+// Create the map object with your mapId and token,
+// referencing the DOM element where you want the map to go.
+L.mapbox.accessToken = accessToken;
+var map = L.mapbox.map('map', mapId);
+
+// Set the initial view of the map to the whole US
+map.setView([39, -96], 4);
+
+// Great, now we have a basic web
 
 var dataFileToAdd = 'data/parks.geojson';
 
@@ -14,3 +32,8 @@ featureLayer.on('ready', function() {
     });
     map.fitBounds(featureLayer.getBounds());
 });
+featureLayer.on('ready',function(){
+  this.eachLayer(function(layer){
+    layer.bindPopup('Welcome to'+layer.feature.properties.LABEL);
+  })
+})
