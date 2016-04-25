aeb2a1c6adb6bb1cd04790e4efb2e3346327a18b
bartonj <jbart_26@yahoo.com>
2016-04-25 09:51:31 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -2,17 +2,20 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'bartonj.pp97f9mf';
-
+var mapId = 'will-breitkreutz.k6fj4l3f';
 
 // And this is my access token, use yours.
-var accessToken = 'pk.eyJ1IjoiYmFydG9uaiIsImEiOiJjaW5nMno2OXkxYjZpdjhrcW55ZjRiZmtwIn0.1zoT87L8Uh9aRd2aMbOT6Q';
+var accessToken = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw';
 
-// Create the map object with your mapId and token, 
+// Create the map object with your mapId and token,
 // referencing the DOM element where you want the map to go.
 L.mapbox.accessToken = accessToken;
-
 var map = L.mapbox.map('map', mapId);
+
+// Set the initial view of the map to the whole US
+map.setView([39, -96], 4);
+
+// Great, now we have a basic web map!
 
 var dataFileToAdd ='data/parks.geojson';
 var featureLayer=L.mapbox.featureLayer();
